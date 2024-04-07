80d04f5b0f2b12c99a53203f8bf0f5fd079e9cbb
Ritesh672 <riteshyad672@gmail.com>
2024-04-07 22:56:04 +0530

    complete goggle sign in

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import env from "dotenv";
 
 // Define the app
@@ -28,15 +29,15 @@ app.use(express.static("public"));
 
 // Session middleware
 app.use(session({
-    secret: process.env.SESSION_SECRET, // the key to store the user login info
-    resave: false, // this is used to store the data on the database here is false cause we are not saving it 
-    saveUninitialized: true, // to store in the server memory
-    cookie : {
-        maxAge : 1000 * 60 * 60 * 24
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
     }
 }));
 
-// Passport after the session middleware
+// Passport initialization
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -45,10 +46,12 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+// login request for the website
 app.get("/login", (req, res) => {
     res.render("index.ejs");
 });
 
+// the get request so the user can directly go to profile after authenticating
 app.get("/profile", (req, res) => {
     console.log(req.user);
     if (req.isAuthenticated()) {
@@ -57,6 +60,17 @@ app.get("/profile", (req, res) => {
         res.redirect("/login");
     }
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+
+app.get("/auth/google/profile", passport.authenticate("google", {
+    successRedirect: "/profile",
+    failureRedirect: "/login"
+}));
 
 // The register route 
 app.get("/register", (req, res) => {
@@ -70,7 +84,7 @@ app.post("/register", async (req, res) => {
     if (!username || !password) {
         return res.render("register.ejs", { msg: "Username and password are required" });
     }
-         // try catch block to register the user and redirect to the profile page
+    // try catch block to register the user and redirect to the profile page
     try {
         // Check if the user already exists
         const existUser = await db.query("SELECT username FROM users WHERE username = $1;", [username]);
@@ -80,35 +94,26 @@ app.post("/register", async (req, res) => {
         } //error if the same username 
 
         // Hash the password
-        bcrypt.hash(password, saltRounds, async (err, hash) => {  //use the bcrypt and store the hashed password of the user in the database
-            if (err) {
-                console.error("Error while generating hash value:", err);
-                return res.status(500).send("Error while generating hash value");
-            }
-            try {
-                // Insert user data into the database
-                const  newUser =  await db.query ( "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *;", [username, hash]);
-                console.log(newUser.rows[0]);
+        const hash = await bcrypt.hash(password, saltRounds);
 
-                // redirecting the user to the login of the home page
-                if (newUser.rows.length > 0){
-               
-                    const user = newUser.rows[0];   // the user data that the user enter 
-                    req.login(user, (err)=>   // the login and authentication of the user
-                    {
-                        console.log(err);
-                        res.redirect("/profile");
+        // Insert user data into the database
+        const newUser = await db.query("INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *;", [username, hash]);
+        console.log(newUser.rows[0]);
 
-                    });
+        // redirecting the user to the login of the home page
+        if (newUser.rows.length > 0) {
+            const user = newUser.rows[0];   // the user data that the user enter 
+            req.login(user, (err) => {   // the login and authentication of the user
+                if (err) {
+                    console.error("Error during login:", err);
+                    return res.status(500).send("Error during login");
                 }
-            } catch (err) {
-                console.error("Error while inserting the user's data:", err);
-                res.status(500).send("Error while inserting the user's data");
-            }
-        });
+                res.redirect("/profile");
+            });
+        }
     } catch (err) {
-        console.error("Error while handling the SQL request:", err);
-        res.status(500).send("Error while handling the SQL request");
+        console.error("Error while registering user:", err);
+        res.status(500).send("Error while registering user");
     }
 });
 
@@ -117,7 +122,7 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/login",
 }));
 
-passport.use(new Strategy(async function verify(username, password, cb) {
+passport.use("local", new Strategy(async function (username, password, cb) {
     try {
         // Query to select the hashed password from the database
         const userData = await db.query("SELECT * FROM users WHERE username = $1", [username]);
@@ -126,19 +131,14 @@ passport.use(new Strategy(async function verify(username, password, cb) {
             const hashPassword = userData.rows[0].password;
 
             // Use bcrypt to compare the provided password with the hashed password
-            bcrypt.compare(password, hashPassword, function (err, result) {
-                if (err) {
-                    // Handle error
-                    return cb(err);
-                }
-                if (result) {
-                    // If passwords match, return the user
-                    return cb(null, userData.rows[0]);
-                } else {
-                    // If passwords don't match, return false
-                    return cb(null, false);
-                }
-            });
+            const match = await bcrypt.compare(password, hashPassword);
+            if (match) {
+                // If passwords match, return the user
+                return cb(null, userData.rows[0]);
+            } else {
+                // If passwords don't match, return false
+                return cb(null, false);
+            }
         } else {
             // If user is not found in the database, return false
             return cb(null, false);
@@ -150,16 +150,39 @@ passport.use(new Strategy(async function verify(username, password, cb) {
     }
 }));
 
-passport.serializeUser((user, cb)=>
-{
+// Configure Google Strategy
+passport.use("google", new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/profile",
+},
+    async (accessToken, refreshToken, profile, cb) => {
+        // Google authentication logic
+        console.log(profile);
+        // You need to implement saving or retrieving user from the database based on 'profile' data.
+        // This function should call 'cb(null, user)' if authentication succeeds or 'cb(err)' if it fails.
+        try {
+            const result = await db.query("SELECT * FROM users WHERE username = $1", [profile.email]);
+            if (result.rows.length === 0) {
+                const newUser = await db.query("INSERT INTO users (username, password) VALUES ($1, $2);", [profile.email, "google"]);
+                cb(null, newUser.rows[0]);
+            } else {
+                // already existing user
+                cb(null, result.rows[0])
+            }
+        } catch (err) {
+            cb(err)
+        }
+    }
+));
+
+passport.serializeUser((user, cb) => {
     cb(null, user);
 });
 
-passport.deserializeUser((user, cb)=>
-{
+passport.deserializeUser((user, cb) => {
     cb(null, user);
 });
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
